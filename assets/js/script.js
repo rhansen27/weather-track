320760f2d26438fcac8326e4ee71e99551d142db
rph <rhansen0127@gmail.com>
2024-07-22 20:50:00 -0500

    added function for getting weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,49 @@ async function fetchCityInfo(city) {
     throw new Error("error");
   }
 }
+
+async function fetchWeatherInfo(lat, lon) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    );
+    const data = await response.json();
+
+    const currentCity = {
+      name: data.city.name,
+      country: data.city.country,
+      forecast: [],
+      hunidity: data.list[0].main.humidity + "%",
+      temp: Math.floor(data.list[0].main.temp - 273.15) * 1.8 + 32 + "°F",
+      wind: data.list[0].wind.speed + "MPH",
+    };
+
+    let confirmedDay = 1;
+
+    for (let i = 0; i < data.list.length; i++) {
+      if (
+        !dayList.includes(dara.list[i].dt_txt.replace(/\s\d+:\d+:\d+/g, ""))
+      ) {
+        currentCity.forecast[`day${confirmedDay}`] = [
+          {
+            temp:
+              Math.floor((data.list[i].main.temp - 273.15) * 1.8 + 32) + "°F",
+            humidity: data.list[i].main.humidity + "%",
+            wind: data.list[i].wind.speed + "MPH",
+            icon: data.list[i].weather[0].icon,
+            date: data.list[i].dt_txt.replace(/\s\d+:\d+:\d+/g, ""),
+          },
+        ];
+
+        dayList.push(data.list[i].dt_txt.replace(/\s\d+:\d+:\d+/g, ""));
+        confirmedDay++;
+      }
+    }
+
+    dayList = [];
+    cityInfo = currentCity;
+    displayForecast();
+  } catch {
+    throw new Error("error");
+  }
+}
